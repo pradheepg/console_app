3ae3239d2714af3a506e5ad05ed6d0a81fca68ea
Pradheep <pradheep1919g@gmail.com>
2025-05-06 23:37:20 +0530

    Dev 06-05-2025 Src2 hotfix

--- src2/handels.java
+++ src2/handels.java
@@ -56,7 +56,6 @@ public class handels {
         student temp = new student(temp_name,temp_Rollnumber,temp_id);
         temp.set_mark(mark);
         student_list.add(temp);
-        sc.close();
     }
 
     private Boolean check_id(int id){
